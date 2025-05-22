bc7fea2f15dcc290d0e81d7b7f03c00079d37a93
yyouseong <53364172+yyouseong@users.noreply.github.com>
2025-05-22 11:43:25 +0900

    Update Chill．．．은 내가 가장 좋아하는 소수.cc

--- 백준/Silver/33850. Chill．．．은 내가 가장 좋아하는 소수/Chill．．．은 내가 가장 좋아하는 소수.cc
+++ 백준/Silver/33850. Chill．．．은 내가 가장 좋아하는 소수/Chill．．．은 내가 가장 좋아하는 소수.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 #define Max(a,b) ((a)>(b)?(a):(b))
 const int MaxP = 200000;
-vector<bool>chk(MaxP + 1);
+vector<bool>chk(MaxP + 1,true);
 
 int main()
 {
@@ -18,26 +18,21 @@ int main()
     for(int i = 1;i <= n;i++) cin>>tile[0][i];
     for(int i = 1;i <= n;i++) cin>>tile[1][i];
 
-    chk[1] = 1;
-	for (int i = 2; i <= MaxP; i++)
-	{
-		if (!chk[i])
-		{
-			for (int j = i + i; j <= MaxP; j += i)
-			{
-				chk[j] = 1;
-			}
-		}
-	}
-
+    chk[1] = 0;
+    for(int i=2;i*i<=MaxP;i++){
+        if(chk[i] == 0) continue;
+        for(int j=i*i;j<=MaxP;j += i)
+            chk[j] = false;
+    }
+    
     vector<int> dp(n+1,0);
-    dp[1] = chk[tile[0][1]+tile[1][1]]?b:a;
+    dp[1] = chk[tile[0][1]+tile[1][1]]?a:b;
     
     for(int i=2;i<=n;i++)
     {
-        int x = chk[tile[0][i-1]+tile[0][i]]?b:a;
-        int y = chk[tile[1][i-1]+tile[1][i]]?b:a;
-        int z = chk[tile[0][i]+tile[1][i]]?b:a;
+        int x = chk[tile[0][i-1]+tile[0][i]]?a:b;
+        int y = chk[tile[1][i-1]+tile[1][i]]?a:b;
+        int z = chk[tile[0][i]+tile[1][i]]?a:b;
         dp[i] = Max(dp[i-1] + z,dp[i-2] + x + y);
     }
     cout<<dp[n];
