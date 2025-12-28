8631aa5374d80e0a01b6a242394625cb5f151bca
yyouseong <53364172+yyouseong@users.noreply.github.com>
2025-12-29 04:14:08 +0900

    [Gold III] Title: Guess, Time: 112 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1248. Guess/Guess.cc
+++ 백준/Gold/1248. Guess/Guess.cc
@@ -10,29 +10,21 @@ bool chk(int cur)
 {
     for (int i = 0; i <= cur; i++)
     {
-        if (matrix[i][cur] == 0 && integers[cur + 1] - integers[i] != 0)
-            return false;
-        if (matrix[i][cur] == 1 && integers[cur + 1] - integers[i] <= 0)
-            return false;
-        if (matrix[i][cur] == -1 && integers[cur + 1] - integers[i] >= 0)
-            return false;
+        if (matrix[i][cur] == 0 && integers[cur + 1] - integers[i] != 0) return false;
+        if (matrix[i][cur] == 1 && integers[cur + 1] - integers[i] <= 0) return false;
+        if (matrix[i][cur] == -1 && integers[cur + 1] - integers[i] >= 0) return false;
     }
     return true;
 }
 
 bool back(int cur, int end)
 {
-    if (cur == end)
-        return true;
+    if (cur == end) return true;
     for (int idx = -10; idx <= 10; idx++)
     {
         inx[cur] = idx;
         integers[cur + 1] = integers[cur] + idx;
-        if (chk(cur))
-        {
-            if (back(cur + 1, end))
-                return true;
-        }
+        if (chk(cur)) if (back(cur + 1, end)) return true;
     }
     return false;
 }
@@ -46,15 +38,11 @@ int main()
     {
         for (int j = i; j < n; j++, k++)
         {
-            if (str[k] == '0')
-                matrix[i][j] = 0;
-            if (str[k] == '+')
-                matrix[i][j] = 1;
-            if (str[k] == '-')
-                matrix[i][j] = -1;
+            if (str[k] == '0') matrix[i][j] = 0;
+            if (str[k] == '+') matrix[i][j] = 1;
+            if (str[k] == '-') matrix[i][j] = -1;
         }
     }
-
     back(0, n);
     for (int i =0;i<n;i++)
         cout << inx[i] << ' ';
