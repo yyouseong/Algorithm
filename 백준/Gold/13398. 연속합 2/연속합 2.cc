a6b22f2f71c60a436c52ef6954eaddd907acc074
yyouseong <53364172+yyouseong@users.noreply.github.com>
2026-01-10 02:41:33 +0900

    [Gold V] Title: 연속합 2, Time: 12 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.cc
+++ 백준/Gold/13398. 연속합 2/연속합 2.cc
@@ -1,75 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+
 using namespace std;
 
-int main()
-{
-    int n, maxN = -100000000, less = 0;
-    cin >> n;
-    vector<int> dp(n + 1, 0), arr(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> arr[i];
-        if (arr[i] > dp[i - 1] + arr[i])
-        {
-            dp[i] = arr[i];
-            less = 0;
-        }
-        else
-        {
-            dp[i] = arr[i] + dp[i - 1];
-            maxN = max(maxN, dp[i] - less);
-            if (arr[i] < 0)
-                less = min(arr[i], less);
-        }
-        dp[i] = max(dp[i - 1] + arr[i], arr[i]);
-        maxN = max(maxN, dp[i]);
-    }
-    int i = 1, maxN2 = -1000, j = n;
-    while (i <= n && arr[i] <= 0)
-        i++;
-    while (j >= 0 && arr[j] <= 0)
-        j--;
-    vector<int> brr(1, 0);
-    queue<int> q;
-    for (; i <= j; i++)
-    {
-        if (arr[i] < 0)
-        {
-            if (brr.back() - arr[i] < 0)
-            {
-                while (!q.empty())
-                {
-                    maxN2 = max(maxN2, brr.back() - q.front());
-                    q.pop();
-                }
-                brr.push_back(0);
-            }
-            else
-            {
-                brr.back() += arr[i];
-                q.push(arr[i]);
-            }
-        }
-        else
-        {
-            brr.back() += arr[i];
-        }
-    }
-    while (!q.empty())
-    {
-        maxN2 = max(maxN2, brr.back() - q.front());
-        q.pop();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
 
-    /*for (int &i : brr)
-        cout << i << ' ';
-    cout << '\n';*/
+    vector<int> L(n);
+    vector<int> R(n);
 
-    for (int i = 1; i < brr.size(); i++)
-        maxN2 = max(maxN2, brr[i] + brr[i - 1]);
-    cout << max(maxN, maxN2);
+    L[0] = a[0];
+    int ans = L[0];
+    
+    for (int i = 1; i < n; i++) {
+        L[i] = max(a[i], L[i-1] + a[i]);
+        ans = max(ans, L[i]);
+    }
+
+    R[n-1] = a[n-1];
+    for (int i = n - 2; i >= 0; i--) {
+        R[i] = max(a[i], R[i+1] + a[i]);
+    }
+    for (int i = 1; i < n - 1; i++) {
+        int temp = L[i-1] + R[i+1];
+        ans = max(ans, temp);
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
